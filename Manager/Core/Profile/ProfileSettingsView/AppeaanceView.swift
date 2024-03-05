d3bce5d3ee45ce913d1514fd8adc813cee8ecb9d
dima <dima@kojem.ru>
2024-03-05 20:33:33 +0300

    commit 9

---
+++ Manager/Core/Profile/ProfileSettingsView/AppeaanceView.swift
@@ -0,0 +1,18 @@
+//
+//  AppeaanceView.swift
+//  Lovly Kids
+//
+//  Created by Дима Кожемякин on 05.03.2024.
+//
+
+import SwiftUI
+
+struct AppeaanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppeaanceView()
+}
